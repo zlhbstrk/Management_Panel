06cdf86f3c085f2a5f0304488b67c562cd73d232
Zeliha Baştürk <zlhbstrk@hotmail.com>
2020-09-27 00:31:43 +0300

    Gerekli dosya ayarları ve düzenlemesi yapıldı.

--- Management_Panel/Startup.cs
+++ Management_Panel/Startup.cs
@@ -15,6 +15,7 @@ namespace Management_Panel
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,20 @@ namespace Management_Panel
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Default}/{action=Index}"
+                );
             });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
